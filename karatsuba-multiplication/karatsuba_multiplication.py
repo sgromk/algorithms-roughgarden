a570b25d10c5abbb095884bdf573866621d402e0
sgromk <167740273+sgromk@users.noreply.github.com>
2025-02-02 02:53:16 -0600

    Implement karatsuba multiplication

--- karatsuba-multiplication/karatsuba_multiplication.py
+++ karatsuba-multiplication/karatsuba_multiplication.py
@@ -1,3 +1,5 @@
+from math import ceil
+
 def karatsuba_mult(multiplicand, multiplier):
     """
     Performs Karatsuba multiplication on two numbers.
@@ -9,26 +11,16 @@ def karatsuba_mult(multiplicand, multiplier):
     Returns:
         int: The product of the two given numbers.
     """
-    if multiplicand < 10 and multiplier < 10:
+    if multiplicand < 10 or multiplier < 10:
         return multiplicand * multiplier
 
-    # Convert int inputs to strings
-    multiplicand = str(multiplicand)
-    multiplier = str(multiplier)
+    n = max(len(str(multiplicand)), len(str(multiplier)))
+    m = ceil(n/2)
 
-    max_len = max(len(multiplicand), len(multiplier))
-    multiplicand = multiplicand.zfill(max_len)
-    multiplier = multiplier.zfill(max_len)
-
-    n = len(multiplicand)
-    m = (n+1) // 2
-
-    print(f"n: {n}, m: {m}")
-    
-    a = multiplicand[:m]
-    b = multiplicand[m:]
-    c = multiplier[:m]
-    d = multiplier[m:]
+    a = multiplicand // (10 ** m)
+    b = multiplicand % (10 ** m)
+    c = multiplier // (10 **m)
+    d = multiplier % (10 ** m)
 
 
     ac = karatsuba_mult(int(a), int(c))
@@ -43,13 +35,6 @@ def karatsuba_mult(multiplicand, multiplier):
 
     ad_bc = ab_cd - ac - bd
 
-    return ac * 10 ** (2 * (n - m)) + ad_bc * 10 ** (n-m) + bd
+    print(f"a: {a}, b: {b}, c: {c}, d: {d}, n: {n}, m: {m}")
 
-
-if __name__ == '__main__':
-
-    # Using the two given numbers for the problem set.
-    num1 = 12
-    num2 = 34
-
-    print(karatsuba_mult(num1, num2))
+    return ac * 10 ** (2 * m) + ad_bc * 10 ** m + bd
